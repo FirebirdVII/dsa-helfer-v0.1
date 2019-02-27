9033568ebfc3271f29f223d1b90ba8a0a15eac6b
FirebirdVII <simon.meyer@stud.uni-bamberg.de>
2019-02-27 21:36:06 +0100

    argh css

--- src/app/music-panel/music-panel.component.ts
+++ src/app/music-panel/music-panel.component.ts
@@ -15,6 +15,10 @@ export class MusicPanelComponent implements OnInit {
     $(document).ready(function () {
       $(".type-youtube").append('<span class="badge badge-youtube" style="background-color: #ff0000;color: white;float: right;padding-left: 2em;">YT</span>');
       $(".type-spotify").append('<span class="badge badge-spotify" style="background-color: #1db954;color: white;float: right;padding-left: 2em;">S</span>');
+
+      
+      $(".badge-youtube").css({"background-color": "#ff0000", "color" : "white", "float" : "right", "padding-left" : "2em"});
+      $(".badge-spotify").css({"background-color": "#ff0000", "color" : "white", "float" : "right", "padding-left" : "2em"});
     });
   }
 }
