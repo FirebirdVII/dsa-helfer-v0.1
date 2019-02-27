fb0ceca7b29cdfb1e88dff515de17c15e51bf684
FirebirdVII <simon.meyer@stud.uni-bamberg.de>
2019-02-27 21:40:55 +0100

    compress css

--- src/app/music-panel/music-panel.component.ts
+++ src/app/music-panel/music-panel.component.ts
@@ -17,8 +17,8 @@ export class MusicPanelComponent implements OnInit {
       $(".type-spotify").append('<span class="badge badge-spotify">S</span>');
 
       
-      $(".badge-youtube").css({"background-color": "#ff0000", "color": "white", "float": "right", "padding-left": "2em"});
-      $(".badge-spotify").css({"background-color": "#ff0000", "color": "white", "float": "right", "padding-left": "2em"});
+      $(".badge-youtube").css({"background-color": "#ff0000", "color": "white", "float": "right"});
+      $(".badge-spotify").css({"background-color": "#1db954", "color": "white", "float": "right"});
     });
   }
 }
