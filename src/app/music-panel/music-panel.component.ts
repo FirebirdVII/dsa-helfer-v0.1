8de3cb1ddcddef5742b594136396d3096fa9b24f
FirebirdVII <simon.meyer@stud.uni-bamberg.de>
2019-02-27 21:47:25 +0100

    color anpassungen

--- src/app/music-panel/music-panel.component.ts
+++ src/app/music-panel/music-panel.component.ts
@@ -17,8 +17,8 @@ export class MusicPanelComponent implements OnInit {
       $(".type-spotify").append('<span class="badge badge-spotify">S</span>');
 
       
-      $(".badge-youtube").css({"background-color": "#ff0000", "color": "white", "float": "right"});
-      $(".badge-spotify").css({"background-color": "#1db954", "color": "white", "float": "right"});
+      $(".badge-youtube").css({"background-color": "#fb5050", "color": "white", "float": "right"});
+      $(".badge-spotify").css({"background-color": "#76b852", "color": "white", "float": "right"});
     });
   }
 }
