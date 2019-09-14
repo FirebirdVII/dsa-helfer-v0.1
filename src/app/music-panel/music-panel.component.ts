602f6514be9824df8d3a740b6fb7b4f8ed0c6ac5
FirebirdVII <simon.meyer@stud.uni-bamberg.de>
2019-09-14 14:31:46 +0200

    Plains + Hell + mehr links

--- src/app/music-panel/music-panel.component.ts
+++ src/app/music-panel/music-panel.component.ts
@@ -14,11 +14,13 @@ export class MusicPanelComponent implements OnInit {
   ngOnInit() {
     $(document).ready(function () {
       $(".type-youtube").append('<span class="badge badge-youtube">YT</span>');
+      $(".type-youtube-playlist").append('<span class="badge badge-youtube-playlist">YT-PL</span>');
       $(".type-spotify").append('<span class="badge badge-spotify">S</span>');
 
-      
-      $(".badge-youtube").css({"background-color": "#fb5050", "color": "white", "float": "right"});
-      $(".badge-spotify").css({"background-color": "#76b852", "color": "white", "float": "right"});
+
+      $(".badge-youtube").css({ "background-color": "#fb5050", "color": "white", "float": "right" });
+      $(".badge-youtube-playlist").css({ "background-color": "#af0000", "color": "white", "float": "right" });
+      $(".badge-spotify").css({ "background-color": "#76b852", "color": "white", "float": "right" });
     });
   }
 }
